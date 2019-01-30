32187c02b0e21b230809444f5727819a9c0533e9
Greg Doud <gpdoud@gmail.com>
2019-01-30 10:48:49 -0500

    Initial commit

--- DNBC5-Assignments/Program.cs
+++ DNBC5-Assignments/Program.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace DNBC5_Assignments {
     class Program {
+
+        static void UseForToSumNumber() {
+            var total = 0;
+            for(var idx = 0; idx < 10; idx++) {
+                total = total + idx;
+            }
+            Console.WriteLine($"Total is {total}");
+        }
+
+        #region Sum numbers from 1 to 100
+        static void SumNumbers1To100() {
+            var total = 0;
+            var counter = 1;
+            while(counter <= 100) {
+                total = total + counter;
+                counter = counter + 1;
+            }
+            Console.WriteLine($"The sum of the numbers 1 to 100 is {total}");
+        }
+        #endregion
+
+        #region Multiply numbers up to 25
+        static void MultiplyNumbers1to25() {
+            var total = 1;
+            var counter = 1;
+            while(counter <= 25) {
+                total = total * counter;
+                counter = counter + 1;
+            }
+            Console.WriteLine($"Multiplying the numbers 1 to 25 totals {total}");
+        }
+        #endregion
+
         static void Main(string[] args) {
+            UseForToSumNumber();
+            //SumNumbers1To100();
+            //MultiplyNumbers1to25();
         }
     }
 }
